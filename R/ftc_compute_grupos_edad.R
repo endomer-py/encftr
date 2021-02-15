8e015c270fc00799246caa1f846601427c67ef71
Daniel de la Rosa <drdsdaniel@gmail.com>
2021-02-15 18:59:49 -0400

    feat: reformulada para utilizar Dmisc::cut3

--- R/ftc_compute_grupos_edad.R
+++ R/ftc_compute_grupos_edad.R
@@ -1,10 +1,12 @@
 #' Grupos de edad
 #'
+#'   `r lifecycle::badge("experimental")`
+#'
 #'   Divide la variable \code{EDAD} en el número o los grupos especificados.
 #'
 #' @param tbl [data.frame]: Conexión a base de datos o data.frame con los datos
 #' @param breaks Puntos de corte para la variable. Vea \code{See also} para más detalles.
-#' @param labels [character]: Nombres para los grupos de la variable.  Vea \code{See also} para más detalles.
+#' @param labels Nombres para los grupos de la variable.  Vea \code{See also} para más detalles.
 #'
 #' @return [data.frame]: Los datos suministrados en el argumento \code{tbl} con
 #'   la variable \code{grupos_edad} adicionada.
@@ -17,12 +19,8 @@
 #' @examples
 #'   encft <- data.frame(EDAD = c(0:100))
 #'   ftc_compute_grupos_edad(encft)
-ftc_compute_grupos_edad <- function(tbl, breaks = 20, labels = NULL){
+ftc_compute_grupos_edad <- function(tbl, breaks = 10, labels = NULL){
   EDAD <- NULL
-  tbl <- tbl %>%
-    dplyr::mutate(grupos_edad = EDAD)
-  if(!is.null(breaks)){
-    tbl <- Dmisc::cut3(tbl, "grupos_edad", breaks, labels)
-  }
-  tbl
+  tbl <- dplyr::mutate(tbl, grupos_edad = EDAD)
+    Dmisc::cut3(tbl, "grupos_edad", breaks, labels = labels)
 }
